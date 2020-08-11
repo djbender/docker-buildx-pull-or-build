3bcb74fd438b57ba258217d43b3843d63317b44e
Derek Bender <170351+djbender@users.noreply.github.com>
2020-08-11 15:30:26 -0500

    fix typo s/_auth/_json

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ default_image_cache="$image:cache"
 image_cache=${6:-$default_image_cache}
 
 living_tag=$7
-docker_config_auth=$8
+docker_config_json=$8
 
 # act will by default change $HOME to /github/home which
 # is not where the buildx cli-plugin is located
@@ -21,16 +21,16 @@ export DOCKER_CONFIG=/root/.docker
 echo Logging in to "${DOCKER_REGISTRY:-Docker Hub}" ...
 docker_login_log=mktmp
 
-if [ "$docker_config_auth" = '' ]; then
-  echo "docker_config_auth was blank so logging in with username and password..."
+if [ "$docker_config_json" = '' ]; then
+  echo "docker_config_json was blank so logging in with username and password..."
   echo "$DOCKER_PASSWORD" | docker login \
     --username "$DOCKER_USERNAME" \
     --password-stdin "$DOCKER_REGISTRY" > $docker_login_log 2>&1 \
     \
     || docker_login_result=$?
 else
-  echo "Using docker_config_auth to authenticate with registry..."
-  jq --null-input --tab "$docker_config_auth" > /root/.docker/config.json
+  echo "Using docker_config_json to authenticate with registry..."
+  jq --null-input --tab "$docker_config_json" > /root/.docker/config.json
   docker login "$DOCKER_REGISTRY" > $docker_login_log 2>&1 \
     || docker_login_result=$?
 fi
